545d92ebff1eb1969a0d16fad1ce9ca0f6f4a407
Albert Bellonch Llargués <albert@itnig.net>
2012-08-21 00:31:01 +0200

    Refactor paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,3 +1,1 @@
-export MAGICK_HOME="/usr/lib/ImageMagick-6.7.8"
-export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib"
-export PATH="$MAGICK_HOME/bin:/Users/albert/code/dotfiles/bin:$PATH"
+export PATH="/Users/albert/code/dotfiles/bin:$PATH"
