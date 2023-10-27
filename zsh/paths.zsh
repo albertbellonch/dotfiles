1c9b34bd27f20448fb21022bea21bfb4d7ed5064
Albert Bellonch Llargués <albert@bellonch.com>
2023-10-27 11:37:48 +0200

    Paths and envs

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -6,12 +6,24 @@ else
   # Own bins
   export PATH="$PATH:/Users/albert/code/dotfiles/bin"
 
-  # Rails bins from Quipu
-  export PATH="$PATH:/Users/albert/code/quipuapp/bin"
-
   # MySQL path
   export PATH="$PATH:/usr/local/mysql/bin"
 
-  # Fix RVM issue
-  export rvmsudo_secure_path=1
+  # Rustup stuff
+  export RUSTUP_HOME="$HOME/.rustup"
+
+  # Cargo stuff
+  export CARGO_HOME="$HOME/.cargo"
+  export PATH="$PATH:$CARGO_HOME/bin"
+  source "$HOME/.cargo/env"
+
+  # Android Studio stuff
+  export ANDROID_HOME=$HOME/Library/Android/sdk
+  export PATH=$PATH:$ANDROID_HOME/tools
+  export PATH=$PATH:$ANDROID_HOME/tools/bin
+  export PATH=$PATH:$ANDROID_HOME/platform-tools
+  export PATH=$PATH:$ANDROID_HOME/emulator
+
+  # Setup Fastlane
+  export PATH="$HOME/.fastlane/bin:$PATH"
 fi
