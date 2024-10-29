855b982db10a029c9749e337b9a1b42de155c2ca
Albert Bellonch Llargués <albert@bellonch.com>
2024-10-29 16:25:09 +0100

    Update

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -8,22 +8,4 @@ else
 
   # MySQL path
   export PATH="$PATH:/usr/local/mysql/bin"
-
-  # Rustup stuff
-  export RUSTUP_HOME="$HOME/.rustup"
-
-  # Cargo stuff
-  export CARGO_HOME="$HOME/.cargo"
-  export PATH="$PATH:$CARGO_HOME/bin"
-  source "$HOME/.cargo/env"
-
-  # Android Studio stuff
-  export ANDROID_HOME=$HOME/Library/Android/sdk
-  export PATH=$PATH:$ANDROID_HOME/tools
-  export PATH=$PATH:$ANDROID_HOME/tools/bin
-  export PATH=$PATH:$ANDROID_HOME/platform-tools
-  export PATH=$PATH:$ANDROID_HOME/emulator
-
-  # Setup Fastlane
-  export PATH="$HOME/.fastlane/bin:$PATH"
 fi
